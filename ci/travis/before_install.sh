584a05b3a62cd8704144af9be84c6106a18341c6
jmckenna <jmckenna@gatewaygeomatics.com>
2020-12-07 15:15:54 -0400

    update Travis to use Bionic

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -10,7 +10,7 @@ sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-testing
 sudo apt-get update
 sudo apt-get install --allow-unauthenticated protobuf-c-compiler libprotobuf-c0-dev bison flex python-lxml libfribidi-dev cmake librsvg2-dev colordiff libpq-dev libpng-dev libjpeg-dev libgif-dev libgeos-dev libfreetype6-dev libfcgi-dev libcurl4-gnutls-dev libcairo2-dev libgdal-dev libproj-dev libxml2-dev python-dev libexempi-dev lcov lftp postgis libharfbuzz-dev gdal-bin ccache curl pyflakes
 sudo apt-get install --allow-unauthenticated libmono-system-drawing4.0-cil mono-mcs
-sudo apt-get install --allow-unauthenticated php5-dev || sudo apt-get install --allow-unauthenticated php7-dev
+sudo apt-get install --allow-unauthenticated php-dev
 sudo apt-get install --allow-unauthenticated libperl-dev
 sudo pip install cpp-coveralls
 sudo pip install -U -r msautotest/requirements.txt
